3d9df4ec57831f270fc22cc2818bc5ab58663b63
Максим Максим <krutbek2@gmail.com>
2026-09-07 21:17:39 +0300

    fix: security update

--- app/database.py
+++ app/database.py
@@ -6,14 +6,8 @@ from sqlalchemy.orm import DeclarativeBase
 
 load_dotenv()
 
-
-DB_USER = os.getenv("DB_USER")
-DB_PASSWORD = os.getenv("DB_PASSWORD")
-DB_NAME = os.getenv("DB_NAME")
-DB_INSIDE_PORT = os.getenv("DB_INSIDE_PORT")
-
 # Строка подключения: postgresql://логин:пароль@хост:порт/имя_базы
-DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@db:{DB_INSIDE_PORT}/{DB_NAME}"
+DATABASE_URL = os.getenv("DB_CONNECTION_STRING")
 
 # 1. Используем асинхронное свойство database_url_async (с протоколом postgresql+asyncpg://)
 engine = create_async_engine(DATABASE_URL, echo=True)
